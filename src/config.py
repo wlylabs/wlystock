e84073561bf405f6c3436d288d99507683ed18dd
wlylabs <wly38347@gmail.com>
2026-07-22 10:29:36 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 
 HF_TOKEN = os.environ.get("HF_TOKEN")
-SD_MODEL_URL = "https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-xl-base-1.0"
+SD_MODEL_URL = "https://router.huggingface.co/models/black-forest-labs/FLUX.1-dev"
 CAPTION_MODEL_URL = "https://router.huggingface.co/hf-inference/models/Salesforce/blip-image-captioning-base"
 
 OUTPUT_DIR = "output"
