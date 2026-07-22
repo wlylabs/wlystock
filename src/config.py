6b52108f03331d57be8456e9e61ece5a1e529fbd
wlylabs <wly38347@gmail.com>
2026-07-22 10:22:19 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 import os
 
 HF_TOKEN = os.environ.get("HF_TOKEN")
-SD_MODEL_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
-CAPTION_MODEL_URL = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-base"
+SD_MODEL_URL = "https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-xl-base-1.0"
+CAPTION_MODEL_URL = "https://router.huggingface.co/hf-inference/models/Salesforce/blip-image-captioning-base"
 
 OUTPUT_DIR = "output"
 MIN_RESOLUTION = (2400, 2400)
